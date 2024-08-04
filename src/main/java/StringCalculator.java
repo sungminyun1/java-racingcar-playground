9610e0851e1923b545d08c5819bccb257c887280
sungminyun <sungmin.yun@nflux.co.kr>
2024-08-05 01:18:49 +0900

    feat: parse string by delimiter
    parse string by default delimiter and custom delimiter

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,3 +1,9 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 public class StringCalculator {
 
     public int calculate(String input) {
@@ -9,5 +15,25 @@ public class StringCalculator {
 
     private boolean validate(String input) {
         return input != null && !input.isEmpty();
+    }
+
+    private List<Integer> parseStringDefaultDelimiter(String input) {
+        return stringArrayToIntegerList(input.split(",|:"));
+    }
+
+    public List<Integer> parseString(String input) {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(input);
+        if(!m.find()){
+            return parseStringDefaultDelimiter(input);
+        }
+        String customDelimiter = m.group(1);
+        String[] tokens= m.group(2).split(customDelimiter);
+        return stringArrayToIntegerList(tokens);
+    }
+
+    private List<Integer> stringArrayToIntegerList(String[] input) {
+        return Arrays.stream(input)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -2,6 +2,9 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,4 +19,23 @@ class StringCalculatorTest {
         assertThat(stringCalculator.calculate(null)).isEqualTo(0);
     }
 
+    @DisplayName("컴마 또는 콜론을 구분자로 문자열을 구분한다.")
+    @Test
+    void testParseString() {
+        assertThat(stringCalculator.parseString("1,2,3"))
+                .isEqualTo(Arrays.asList(1,2,3));
+
+        assertThat(stringCalculator.parseString("1,2:3"))
+                .isEqualTo(Arrays.asList(1,2,3));
+    }
+
+    @DisplayName("custom delimiter 를 이용해서 파싱한다")
+    @Test
+    void testCustomParseString() {
+        assertThat(stringCalculator.parseString("//;\n1;2;3"))
+                .isEqualTo(Arrays.asList(1,2,3));
+
+        assertThat(stringCalculator.parseString("//<\n1<2<3"))
+                .isEqualTo(Arrays.asList(1,2,3));
+    }
 }
